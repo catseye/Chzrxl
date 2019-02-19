968032430ee7cf238c5410a02768052a59b13ecc
Chris Pressey <cpressey@catseye.tc>
2019-02-19 21:08:55 +0000

    Initial import of files into the Chzrxl reference distribution.

---
+++ README.md
@@ -0,0 +1,34 @@
+Chzrxl
+======
+
+![screenshot](https://static.catseye.tc/images/screenshots/Chzrxl.jpg)
+
+This is the reference distribution for **Chzrxl**, a dynamical system
+fluid automaton thing devised by Chris Pressey in 2014.  It consists
+of a set of blobs where each blob is attracted to the midpoint of two
+other blobs.
+
+You can watch it online here: [Chzrxl installation at catseye.tc][].
+(But note that, as of this writing, this is not the version in this
+repository; it is the version from the [HTML5 Gewgaws distribution][]).
+
+Being built on [PixiJS][], the version in this repo has the potential to
+have much nicer visuals; however, it is still a work-in-progress.
+To see it, clone this repository and open
+[demo/chzrxl.html](demo/chzrxl.html) in a web browser such as
+Firefox.
+
+(If opened as a local file, it may fall back to canvas rendering; to
+ensure that WebGL is used, you can [start a local server][], but to be
+frank it doesn't seem to make much difference at this point.)
+
+(TODO: bring this implementation up to par with the other one, and install
+this one online at catseye.tc instead.)
+
+The core behaviour is implemented in [src/chzrxl.js](src/chzrxl.js) and
+does not rely on PixiJS or any other display layer.
+
+[Chzrxl installation at catseye.tc]: https://catseye.tc/installation/Chzrxl
+[HTML5 Gewgaws distribution]: https://catseye.tc/distribution/HTML5%20Gewgaws%20distribution
+[PixiJS]: http://www.pixijs.com/
+[start a local server]: https://github.com/mrdoob/three.js/wiki/How-to-run-things-locally#run-local-server
---
+++ src/chzrxl.js
@@ -0,0 +1,86 @@
+Blob = function() {
+    this.init = function(config) {
+        this.index = config.index;
+        this.x = config.x;
+        this.y = config.y;
+        this.pt1 = config.pt1;
+        this.pt2 = config.pt2;
+        this.radius = config.radius;
+        this.rate = config.rate;
+        this.phase = config.phase;
+        this.colour = config.colour;
+        this.blobs = config.blobs;
+        if (config.onInitBlob) config.onInitBlob(this);
+        return this;
+    };
+
+    this.update = function(t) {
+        // p is between 0.0 (at pt1) and 1.0 (at pt2)
+        var p = (Math.sin((t + this.phase) / this.rate) + 1) / 2;
+
+        var blob1 = this.blobs[this.pt1];
+        var blob2 = this.blobs[this.pt2];
+
+        var x1 = blob1.x;
+        var x2 = blob2.x;
+        this.x = (x2 - x1) * p + x1;
+        var y1 = blob1.y;
+        var y2 = blob2.y;
+        this.y = (y2 - y1) * p + y1;
+    };
+};
+
+
+Chzrxl = function() {
+    this.init = function(config) {
+        this.width = config.width;
+        this.height = config.height;
+        this.blobs = [];
+        this.numBlobs = 200;
+        this.onUpdateBlob = config.onUpdateBlob;
+        this.onInitBlob = config.onInitBlob;
+        this.reset();
+        return this;
+    };
+
+    this.reset = function() {
+        this.blobs = [];
+        this.tick = 0;
+        for (var i = 0; i < this.numBlobs; i++) {
+            var x = Math.random() * this.width;
+            var y = Math.random() * this.height;
+            var pt1 = i;
+            var pt2;
+            while (pt1 === i || pt2 === i || pt2 === pt1) {
+               pt1 = Math.floor(Math.random() * this.numBlobs);
+               pt2 = Math.floor(Math.random() * this.numBlobs);
+            }
+            var rate = Math.random() * 100 + 100;
+            var phase = Math.floor(Math.random() * 110);
+            var radius = Math.floor(Math.random() * 10) + 5;
+            this.blobs.push(new Blob().init({
+                index: i,
+                x: x,
+                y: y,
+                pt1: pt1,
+                pt2: pt2,
+                rate: rate,
+                phase: phase,
+                radius: radius,
+                colour: 0xff0000 + i,
+                blobs: this.blobs,
+                onInitBlob: this.onInitBlob
+            }));
+        }
+    };
+
+    this.update = function() {
+        for (var i = 0; i < this.blobs.length; i++) {
+            if (i < 50) continue;
+            var blob = this.blobs[i];
+            blob.update(this.tick);
+            if (this.onUpdateBlob) this.onUpdateBlob(blob);
+        }
+        this.tick += 1;
+    };
+};
