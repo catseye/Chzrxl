467ba3eb36f34ea325b7d0160823dce703d74b8b
Chris Pressey <cpressey@catseye.tc>
2019-05-24 13:23:31 +0100

    Create UI using DAM.

--- demo/chzrxl-pixi-launcher.js
+++ demo/chzrxl-pixi-launcher.js
@@ -24,67 +24,6 @@ function hslToRgb(h, s, l) {
   );
 }
 
-function makeDiv(container, innerHTML) {
-    var div = document.createElement('div');
-    div.innerHTML = innerHTML || '';
-    container.appendChild(div);
-    return div;
-}
-
-function makeSpan(container, innerHTML) {
-  var span = document.createElement('span');
-  span.innerHTML = innerHTML || '';
-  container.appendChild(span);
-  return span;
-}
-
-function makeLabel(container, innerHTML) {
-    var label = document.createElement('label');
-    label.innerHTML = innerHTML || '';
-    container.appendChild(label);
-    return label;
-}
-
-function makeButton(container, labelText, fun) {
-  var button = document.createElement('button');
-  button.innerHTML = labelText;
-  container.appendChild(button);
-  button.onclick = fun;
-  return button;
-}
-
-function makeSlider(container, min_, max_, value, fun) {
-  var slider = document.createElement('input');
-  slider.type = "range";
-  slider.min = min_;
-  slider.max = max_;
-  slider.value = value || 0;
-  slider.onchange = function(e) {
-    fun(parseInt(slider.value, 10));
-  };
-  container.appendChild(slider);
-  return slider;
-}
-
-function makeSelect(container, labelText, optionsArray, fun) {
-  var label = document.createElement('label');
-  label.innerHTML = labelText;
-  container.appendChild(label);
-  var select = document.createElement("select");
-  for (var i = 0; i < optionsArray.length; i++) {
-    var op = document.createElement("option");
-    op.value = optionsArray[i].value;
-    op.text = optionsArray[i].text;
-    select.options.add(op);
-  }
-  select.onchange = function(e) {
-    fun(optionsArray[select.selectedIndex]);
-  };
-  select.selectedIndex = 0;
-  label.appendChild(select);
-  return select;
-}
-
 function removeVisuals(c) {
   c.forEachBlob(function(blob) {
     if (blob.graphics) {
@@ -166,39 +105,52 @@ function launch(config) {
 
   setClassicVisuals(c, app.stage);
 
-  var controlPanel = makeDiv(config.container);
+  var div=DAM.maker('div'), button=DAM.maker('button'), span=DAM.maker('span');
 
   /*----- renderer panel -----*/
-  var rendererPanel = makeDiv(controlPanel);
-  var rendererSpan = makeSpan(rendererPanel);
+
   var renderer = getRenderer(app);
-  rendererSpan.innerHTML = "Renderer: " + renderer + ".";
+  var rendererSpan = span("Renderer: " + renderer + ".");
+  var forceRendererButton = null;
   if (renderer !== "Canvas") {
-    var forceCanvasButton;
-    forceCanvasButton = makeButton(rendererPanel, "Force Canvas renderer", function(e) {
-      removeVisuals(c);
-      app.destroy(true, true);
-      config.forceCanvas = true;
-      r = setUpPixiApp(config, c);
-      app = r.app;
-      setClassicVisuals(c, app.stage);
-      forceCanvasButton.remove();
-      rendererSpan.innerHTML = "Renderer: Canvas.";
-    });
+    forceRendererButton = button(
+      "Force Canvas renderer",
+      {
+        onclick: function(e) {
+          removeVisuals(c);
+          app.destroy(true, true);
+          config.forceCanvas = true;
+          r = setUpPixiApp(config, c);
+          app = r.app;
+          setClassicVisuals(c, app.stage);
+          forceRendererButton.remove();
+          rendererSpan.innerHTML = "Renderer: Canvas.";
+        }
+      }
+    );
   }
+  var rendererPanel = div(
+    rendererSpan,
+    forceRendererButton
+  );
 
   /*----- visuals panel -----*/
-  var visualsPanel = makeDiv(controlPanel);
-  makeSelect(visualsPanel, "Visuals:", [
-    { text: "Classic", value: "1", setVisuals: setClassicVisuals },
-    { text: "Noisy", value: "2", setVisuals: setNoiseVisuals }
-  ], function(selection) {
-    removeVisuals(c);
-    selection.setVisuals(c, app.stage);
-  });
+  var visualsPanel = div(
+    DAM.makeSelect(
+      {
+        title: "Visuals:",
+        options: [
+          { text: "Classic", value: "1", setVisuals: setClassicVisuals },
+          { text: "Noisy", value: "2", setVisuals: setNoiseVisuals }
+        ],
+        onchange: function(option) {
+          removeVisuals(c);
+          option.setVisuals(c, app.stage);
+        }
+      }
+    ),
+    DAM.makeRange({ title: "Percent to hold fixed:", min: 0, max: 100, value: 20, onchange: function(v) { c.setPercentToHoldFixed(v); } })
+  );
 
-  var label = makeLabel(controlPanel, "Percent to hold fixed:");
-  makeSlider(label, 0, 100, 20, function(v) {
-    c.setPercentToHoldFixed(v);
-  });
+  config.container.appendChild(div(rendererPanel, visualsPanel));
 }
